1d0f6b8aea2623b5d8a1d4a96600e95a6e31ea90
sivashanmugam Thangavel <sivashanmugam@gmail.com>
2022-11-10 12:30:12 -0700

    Added Portal changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "presolvedadmin": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
